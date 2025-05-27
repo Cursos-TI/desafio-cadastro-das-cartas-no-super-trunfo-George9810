67ea855ecd5d0c675a9df4a0b9e6231ca2c73ba7
George9810 <georgenegaodssantos@gmail.com>
2025-05-27 02:03:06 +0000

    Printf Super  trunfo

---
+++ Cartas Super trunfo/C Cartas Super Trunfo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main(){
+
+    char  Samambaia[10];
+    int  Populacão;
+    float Área;
+    float Pib;
+    int   Pontos;
+
+    
+    
+    char  Brasília2[9];
+    int   Populacão2;
+    float Área2;
+    float Pib2;
+    int   Turísticos2;
+
+
+    printf ("digite o nome da cidade ( 10 ): " );
+    scanf  ("%s ,&nome da cidade e  Samambaia ");
+
+    printf ("digite a populacão da cidade: " );
+    scanf  ("%d , &populacão e de 245 mil pessoas" );
+
+    printf ("digite a área em km²: " );
+    scanf  ("%f , &a área e de 102,6km² " );
+
+    printf ("digite o pib de samamabaia: " );
+    scanf  ("%f , o Pib de samamabaia e de 992,30 reais " );
+
+    printf ("digite os pontos turistico de samambaia e a: " );
+    scanf  ("%d, igreja da barca " );
+
+
+    printf ("digite o nome da cidade (09): " );
+    scanf  ("%s &nome da cidade e Brasilia" );
+    
+    printf ("digite a populacão da cidade: " );
+    scanf  ("%d &a populacão da cidade e 1 milhão de abitantes: " );
+
+    printf ("digite uma área km²: " );
+    scanf  ("%f &a area e de 5,761km² " );
+
+    printf ("digite o pib de brasilia: " );
+    scanf  ("%f &o pib de brasilia e de 2,174 trilhões" );
+
+    printf ("digete um ponto turistico de bradilia: " );
+    scanf  ("%d &o ponto turistico de brasilia e a catedral" );
+    
+    
+    return 0;
+
+}
